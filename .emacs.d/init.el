bc353e6e1148c77555240e18163abaa57b25e14c
mats cronqvist <masse@cronqvi.st>
2019-06-20 10:49:38 +0200

    emacs font tweak

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -203,5 +203,5 @@ Repeated invocations toggle between the two most recently open buffers."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "#f9f5d7" :foreground "#282828" :weight normal :height 200 :family "ProggyCleanTT"))))
+ '(default ((t (:background "#f9f5d7" :foreground "#282828" :weight normal :height 150 :family "ProggyCleanTT"))))
  '(magit-section-highlight ((t (:background "chartreuse")))))
